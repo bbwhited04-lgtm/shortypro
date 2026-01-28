87b18f09aa729f6ec634caf3c5d0e3d1e3ffe581
bbwhited04 <bbwhited@icloud.com>
2026-01-27 22:50:39 -0600

    Fix invalid page.tsx unicode escape

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,3 @@
-import { redirect } from "next/navigation";\n\nexport default function Home(){\n  redirect("/index.html");\n}\n
 import { redirect } from "next/navigation";
 
 export default function Home() {
